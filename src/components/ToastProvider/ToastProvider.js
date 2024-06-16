7ea01abe2f7625098dc8bcfbc32dbaf503b528ce
owolabiezekiel <owo.ezekiel@gmail.com>
2024-06-16 19:15:50 +0100

    Keyboard and screen reader support

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -25,6 +25,20 @@ function ToastProvider({ children }) {
     setToasts(nextToasts);
   }
 
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key === "Escape") {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <ToastContext.Provider value={{ toasts, createToast, dismissToast }}>
       {children}
